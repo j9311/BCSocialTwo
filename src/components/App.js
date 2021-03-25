2e47d7747e15f4ea21b601707d2d2567dfae7505
James Post <avilapost93@gmail.com>
2021-03-25 16:00:16 -0700

    front end nav

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
-
+import Navbar from './Navbar'
 
 
 
@@ -35,6 +35,14 @@ async loadBlockChainData() {
   //load account
   const accounts = await web3.eth.getAccounts()
   console.log(accounts)
+  this.setState({account: accounts[0]})
+}
+
+constructor(props) {
+  super(props)
+  this.state = {
+    account: ''
+  }
 }
 
 
@@ -42,39 +50,11 @@ async loadBlockChainData() {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <Navbar account={this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
               </div>
             </main>
           </div>
